27a08a98591569b6408ce24fd4b5273d4d5f9f69
Kyota Poeti <kyota.poeti@icloud.com>
2021-12-15 08:40:23 -0500

    Add comments to InitializePaging in Kernel.cpp

--- kernel/src/Kernel.cpp
+++ kernel/src/Kernel.cpp
@@ -20,25 +20,32 @@ extern uint64_t kernelEnd;
 
 void InitializePaging(BootInfo* bootInfo)
 {
+    // Request a page to store the kernel PLM4 and fill it with 0
     PageTable* plm4 = (PageTable*) globalPageFrameAllocator.RequestPage();
     memset(plm4, 0, 0x1000);
 
+    // Initialize kernel PLM4 so that it is mapped linearly (virt 0x1000 -> phys 0x1000, virt 0x3237 -> phys 0x3237, etc.)
     PageTableManager pageTableManager = PageTableManager(plm4);
     uint64_t memMapEntryCount = bootInfo->memMapSize / bootInfo->memDescSize;
     uint64_t totalMemory = GetTotalMemorySize(bootInfo->memMap, memMapEntryCount, bootInfo->memDescSize);
     for (uint64_t i = 0; i < totalMemory; i += 4096)
     {
-        pageTableManager.MapMemory((void *) i, (void *) i);
+        pageTableManager.MapMemory((void*) i, (void*) i);
     }
 
     uint64_t framebufferBase = (uint64_t)bootInfo->framebuffer->baseAddress;
     uint64_t framebufferSize = (uint64_t)bootInfo->framebuffer->bufferSize;
+
+    // Lock the pages for the framebuffer to prevent it from being overridden
     globalPageFrameAllocator.LockPages((void*)framebufferBase, framebufferSize / 4096 + 1);
+
+    // Make sure the virt address for the framebuffer is identical to its physical address
     for (uint64_t i = framebufferBase; i < framebufferBase + framebufferSize; i += 4096)
     {
         pageTableManager.MapMemory((void*)i, (void*)i);
     }
 
+    // Activate paging
     asm("mov %0, %%cr3" : : "r" (plm4));
 }
 
